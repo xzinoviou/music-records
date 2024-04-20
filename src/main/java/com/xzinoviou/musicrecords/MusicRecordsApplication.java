0805950dc3dbc69490f00ee0cd7f271854b79222
Xenofon Zinoviou <xzinoviou@gmail.com>
2024-04-20 11:52:34 +0300

    initial commit

---
+++ src/main/java/com/xzinoviou/musicrecords/MusicRecordsApplication.java
@@ -0,0 +1,13 @@
+package com.xzinoviou.musicrecords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicRecordsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicRecordsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xzinoviou/musicrecords/MusicRecordsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xzinoviou.musicrecords;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicRecordsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
